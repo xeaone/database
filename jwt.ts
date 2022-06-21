a250b6ec0a8612580503c2ef81b10b1cb34f66eb
Alexander Elias <alex.steven.elias@gmail.com>
2022-06-21 14:51:27 -0700

    updated var names

--- jwt.ts
+++ jwt.ts
@@ -16,16 +16,16 @@ export type Payload = {
 
 const encoder = new TextEncoder();
 
-export default async function (header: Header, payload: Payload, key: string): Promise<string> {
+export default async function (header: Header, payload: Payload, secret: string): Promise<string> {
     const encodedHeader = base64url.encode(JSON.stringify(header));
     const encodedPayload = base64url.encode(JSON.stringify(payload));
     const data = encoder.encode(`${encodedHeader}.${encodedPayload}`);
 
-    const cleanedKey = key.replace(/^\n?-----BEGIN PRIVATE KEY-----\n?|\n?-----END PRIVATE KEY-----\n?$/g, '');
+    const cleanedKey = secret.replace(/^\n?-----BEGIN PRIVATE KEY-----\n?|\n?-----END PRIVATE KEY-----\n?$/g, '');
     const decodedKey = base64.decode(cleanedKey).buffer;
-    const rsaKey = await crypto.subtle.importKey('pkcs8', decodedKey, { name: 'RSASSA-PKCS1-v1_5', hash: 'SHA-256' }, true, [ 'sign' ]);
+    const key = await crypto.subtle.importKey('pkcs8', decodedKey, { name: 'RSASSA-PKCS1-v1_5', hash: 'SHA-256' }, true, [ 'sign' ]);
 
-    const signature = await crypto.subtle.sign({ hash: { name: 'SHA-256' }, name: 'RSASSA-PKCS1-v1_5' }, rsaKey, data);
+    const signature = await crypto.subtle.sign({ hash: { name: 'SHA-256' }, name: 'RSASSA-PKCS1-v1_5' }, key, data);
     const encodedSignature = base64url.encode(signature);
     return `${encodedHeader}.${encodedPayload}.${encodedSignature}`;
 }
