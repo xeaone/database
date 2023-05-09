f2c06baf8665f558ed594ac92991444624068ce7
Alexander Elias <alex.steven.elias@gmail.com>
2023-05-09 15:28:28 -0700

    2.1.3

--- utl/deploy.ts
+++ utl/deploy.ts
@@ -6,19 +6,21 @@ const [release] = args;
 
 if (!release) throw new Error('release required');
 
+const pkg = JSON.parse(await readTextFile('./package.json'));
+pkg.version = increment(pkg.version, release as ReleaseType);
+
+if (!pkg.version) throw new Error('release not valid');
+
 const g = await (new Deno.Command('git', { args: ['fetch'] })).output();
 if (!g.success) throw new Error('git auth');
 
 const n = await (new Deno.Command('npm', { args: ['whoami'] })).output();
 if (!n.success) throw new Error('npm auth');
 
-const pkg = JSON.parse(await readTextFile('./package.json'));
-pkg.version = increment(pkg.version, release as ReleaseType);
-
 await writeTextFile('./package.json', JSON.stringify(pkg, null, '    '));
 
-await (new Deno.Command('git', { args: ['commit', '-a', '-m', pkg.version] })).output();
-await (new Deno.Command('git', { args: ['push'] })).output();
-await (new Deno.Command('git', { args: ['tag', pkg.version] })).output();
-await (new Deno.Command('git', { args: ['push', '--tag'] })).output();
-await (new Deno.Command('npm', { args: ['publish', '--access', 'public'] })).output();
+await (new Deno.Command('git', { args: ['commit', '-a', '-m', pkg.version] })).spawn();
+await (new Deno.Command('git', { args: ['push'] })).spawn();
+await (new Deno.Command('git', { args: ['tag', pkg.version] })).spawn();
+await (new Deno.Command('git', { args: ['push', '--tag'] })).spawn();
+await (new Deno.Command('npm', { args: ['publish', '--access', 'public'] })).spawn();
