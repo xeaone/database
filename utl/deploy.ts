7a079503da9224f1426b802bd9dcab415850b79f
Alexander Elias <alex.steven.elias@gmail.com>
2023-05-09 15:19:39 -0700

    null

--- utl/deploy.ts
+++ utl/deploy.ts
@@ -6,8 +6,8 @@ const [release] = args;
 
 if (!release) throw new Error('release required');
 
-const f = await (new Deno.Command('git', { args: ['fetch'] })).output();
-if (!f.success) throw new Error('git auth');
+const g = await (new Deno.Command('git', { args: ['fetch'] })).output();
+if (!g.success) throw new Error('git auth');
 
 const n = await (new Deno.Command('npm', { args: ['whoami'] })).output();
 if (!n.success) throw new Error('npm auth');
@@ -16,6 +16,7 @@ const pkg = JSON.parse(await readTextFile('./package.json'));
 pkg.version = increment(pkg.version, release as ReleaseType);
 
 await writeTextFile('./package.json', JSON.stringify(pkg, null, '    '));
+
 await (new Deno.Command('git', { args: ['commit', '-a', '-m', pkg.version] })).output();
 await (new Deno.Command('git', { args: ['push'] })).output();
 await (new Deno.Command('git', { args: ['tag', pkg.version] })).output();
