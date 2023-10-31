36faa0ff0f902418c865e13b9fcfc102f40b8cf4
Alexander Elias <alex.steven.elias@gmail.com>
2023-10-31 14:26:45 -0700

    dep update

--- src/jwt.ts
+++ src/jwt.ts
@@ -1,5 +1,5 @@
-import * as base64url from 'https://deno.land/std@0.204.0/encoding/base64url.ts';
-import * as base64 from 'https://deno.land/std@0.204.0/encoding/base64.ts';
+import { encodeBase64Url } from 'https://deno.land/std@0.204.0/encoding/base64url.ts';
+import { decodeBase64 } from 'https://deno.land/std@0.204.0/encoding/base64.ts';
 
 export type Header = {
     alg: 'RS256';
@@ -17,15 +17,15 @@ export type Payload = {
 const encoder = new TextEncoder();
 
 export default async function (header: Header, payload: Payload, secret: string): Promise<string> {
-    const encodedHeader = base64url.encode(JSON.stringify(header));
-    const encodedPayload = base64url.encode(JSON.stringify(payload));
+    const encodedHeader = encodeBase64Url(JSON.stringify(header));
+    const encodedPayload = encodeBase64Url(JSON.stringify(payload));
     const data = encoder.encode(`${encodedHeader}.${encodedPayload}`);
 
     const cleanedKey = secret.replace(/^\n?-----BEGIN PRIVATE KEY-----\n?|\n?-----END PRIVATE KEY-----\n?$/g, '');
-    const decodedKey = base64.decode(cleanedKey).buffer;
+    const decodedKey = decodeBase64(cleanedKey).buffer;
     const key = await crypto.subtle.importKey('pkcs8', decodedKey, { name: 'RSASSA-PKCS1-v1_5', hash: 'SHA-256' }, true, ['sign']);
 
     const signature = await crypto.subtle.sign({ hash: { name: 'SHA-256' }, name: 'RSASSA-PKCS1-v1_5' }, key, data);
-    const encodedSignature = base64url.encode(signature);
+    const encodedSignature = encodeBase64Url(signature);
     return `${encodedHeader}.${encodedPayload}.${encodedSignature}`;
 }
